03f3f1a0a8c9dc88ad79597aebbcc137cace2565
Kyle <kdarling95@yahoo.com>
2018-08-14 16:35:51 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Dynamic Multiplayer Text Board
+
+# Description
+
+A hackable program that allows for the creation of several player "boards" in which are passed currently
+a 2D integer array, geneset, and score to be displayed per board.
+The programs intent is to allow for Python to call it as a method helper in displaying the GUI screens derived from the 2D board being
+utilized inside each spawned thread of there respective Genetic Algorithm.
+
+The global fields present inside PythonGUI allow for personal customization of display size and currently working on buffer spaces in
+between each board. The simple equations in place allow for varying columns and sizes.
+
+# Fields - Important Ones Listed
+
+numberOfPlayers (Integer) - Allows for creation of n players onto the board to be updated and maintained.
+boardWidth (Integer) - Designated size per board width.
+boardHeight (Integer) - Designated size per board height.
+expander (Integer) - Multiplies size of board.
+drawCols (Integer) - Number of columns per row to draw until a new row is to be added for further additions.
+
+# Constructors
+
+@param int numberOfPlayers - Initializes numberOfPlayers field.
+@param int boardWidth - Initializes number of columns/total width of board before multiplier is added.
+@param int boardHeight - Initializes number of rows/total height of board before multiplier is added. 
+PythonGUI - Constructs GUI screen with prescreen detail of the board.
+
+@param int width - Used to know total width after multiplier is added.
+@param int height - Used to know total height after multiplier is added.
+@param other - Set as 0, not in use currently.
+Board - Constructs base board system for specified board.
+
+# Methods
+
+@param int index - Used to retrieve specified board for further use.
+getBoard
+
+@param int[][] board - 2D integer array to be displayed.
+@param int[] genes - Genes from the genetic algorithm to be displayed.
+@param int score - Score to be displayed from genetic algorithm.
+draw - Draws board, genes, and score for respective board position given constructors parameters
+
---
+++ src/com/iamabszol/PythonGUI.java
@@ -0,0 +1,195 @@
+package com.iamabszol;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PythonGUI {
+
+    private int numberOfPlayers = 0;
+    private int boardWidth = 0;
+    private int boardHeight = 0;
+
+    private Board[] myBoards = null;
+    private final int cellBuffer = 0;
+    private final int boardBuffer = 0;
+    private final int expander = 10;
+    private final int drawCols = 10;
+
+    private JFrame frame = null;
+    private JPanel panel = null;
+
+    private int[][] text_boards = null;
+    private int[] test_genes = null;
+
+    public PythonGUI(int numberOfPlayers, int boardWidth, int boardHeight) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+
+        constructGUI();
+        apply_test();
+    }
+
+    private void constructGUI() {
+
+        int widthPlayers = 0;
+        int heightPlayers = 0;
+        if(numberOfPlayers % drawCols == 0) {
+            widthPlayers = drawCols;
+            heightPlayers = (numberOfPlayers / drawCols) - 1;
+        } else {
+            if(numberOfPlayers < drawCols) {
+                widthPlayers = numberOfPlayers;
+                heightPlayers = 0;
+            } else {
+                widthPlayers = drawCols;
+                heightPlayers = numberOfPlayers / drawCols;
+            }
+        }
+
+        frame = new JFrame("Python GUI - Tetris Players");
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setSize(new Dimension(boardWidth * expander + ((widthPlayers - 1) * boardWidth * expander), boardHeight * expander + (heightPlayers * boardHeight * expander)));
+
+        myBoards = new Board[numberOfPlayers];
+
+        int row = 0;
+        for(int i = 0; i < myBoards.length; i++) {
+            myBoards[i] = new Board(boardWidth * expander, boardHeight * expander, cellBuffer, boardBuffer);
+            panel.add(myBoards[i]);
+            if(i % drawCols == 0 && i != 0) {
+                row++;
+            }
+            myBoards[i].setBounds((i % drawCols) * boardWidth * expander, row * boardHeight * expander, boardWidth * expander, boardHeight * expander);
+        }
+
+        frame.add(panel);
+        frame.setSize(new Dimension(boardWidth * expander + ((widthPlayers - 1) * boardWidth * expander), boardHeight * expander + (heightPlayers * boardHeight * expander)));
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+    }
+
+    private void apply_test() {
+
+        ArrayList<int[][]> master = new ArrayList<>();
+        for(int i = 0; i < myBoards.length; i++) {
+            text_boards = new int[boardHeight][boardWidth];
+            for(int r = 0; r < text_boards.length; r++) {
+                for(int c = 0; c < text_boards[r].length; c++) {
+                    text_boards[r][c] = 1;
+                }
+            }
+
+            test_genes = new int[4];
+            for(int g = 0; g < test_genes.length; g++) {
+                test_genes[g] = new Random().nextInt(10);
+            }
+            myBoards[i].draw(text_boards, test_genes, 0);
+        }
+    }
+
+    class Board extends JPanel {
+
+        int width = 0;
+        int height = 0;
+
+        int[][] board = null;
+        int[] genes = null;
+        int score = 0;
+
+        public Board(int width, int height, int cellBuffer, int boardBuffer) {
+            this.width = width;
+            this.height = height;
+            setLayout(null);
+            setSize(width, height);
+        }
+
+        // python literally has max() to do this =[
+        private int getMaxRow() {
+            int[] results = {0,0};
+            for(int r = 0; r < board.length; r++) {
+                int sum = 0;
+                for(int c = 0; c < board[r].length; c++) {
+                    sum += (int) board[r][c];
+                }
+                if(sum > results[0]) {
+                    results[1] = r;
+                }
+            }
+            return results[1];
+        }
+
+        public void draw(int[][] board, int[] genes, int score) {
+            this.board = board;
+            this.genes = genes;
+            this.score = score;
+            repaint();
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Color[] colors = new Color[]{Color.red, Color.green, Color.blue, Color.black, Color.yellow, Color.pink, Color.cyan, Color.orange};
+            int[] sizes = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+            g.setColor(Color.white);
+            g.fillRect(0, 0, width, height);
+            g.setColor(colors[new Random().nextInt(colors.length)]);
+            g.drawRect(0, 0, width - 1, height - 1);
+            if(board != null && genes != null) {
+                // calculate appropriate font size (allows for dynamic changing)
+                int calcRow = getMaxRow();
+                int bestSize = 0;
+                for (int s = 0; s < sizes.length; s++) {
+                    g.setFont(new Font("Arial", Font.BOLD, sizes[s]));
+                    String line = "";
+                    for (int i = 0; i < board[calcRow].length; i++) {
+                        line = line + " " + board[calcRow][i];
+                    }
+                    if (g.getFontMetrics().stringWidth(line) < width && (g.getFontMetrics().getHeight() * (boardHeight + 3)) < height) {
+                        bestSize = sizes[s];
+                    } else
+                        break;
+                }
+                g.setFont(new Font("Arial", Font.BOLD, bestSize));
+                g.setColor(Color.black);
+                int row = g.getFontMetrics().getHeight();
+                int col = 0;
+                for (int r = 0; r < board.length; r++) {
+                    String line = "";
+                    for (int c = 0; c < board[r].length; c++) {
+                        //g.drawString("" + board[r][c], col, row);
+                        //col += g.getFontMetrics().stringWidth("" + board[r][c]) + cellBuffer;
+                        line = line + " " + board[r][c];
+                    }
+                    g.drawString(line, col, row);
+                    row += g.getFontMetrics().getHeight();
+                }
+                // draw genes if there's enough room
+                if(row < (height - g.getFontMetrics().getHeight())) {
+                    String line = "[";
+                    for(int c = 0; c < genes.length; c++) {
+                        line = line + genes[c] + ",";
+                    }
+                    line = line.substring(0, line.length() - 1) + "]";
+                    g.drawString("Genes: " + line, 0, row);
+                    row += g.getFontMetrics().getHeight();
+                    g.drawString("Score: " + score, 0, row);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PythonGUI pg = new PythonGUI(12, 10, 22);
+    }
+
+}
